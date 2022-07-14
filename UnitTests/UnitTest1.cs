ecbfc8438d52e73494bb8659e1031a26ae5dd775
RSW <rswpub@gmail.com>
2022-07-14 10:08:51 -0600

    Building the tests

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -3,9 +3,15 @@ namespace UnitTests
     public class ProvideArrayOfIntegers
     {
         [Theory]
-        [InlineData()]
-        public void ReturnsMaxArea()
+        [InlineData(new int[] {1,8,6,2,5,4,8,3,7 }, 49)]
+        public void ReturnsMaxArea(int[] heightArray, int expectedResult)
         {
+
+            // Arrange
+
+            // Act
+
+            // Assert
 
         }
     }
