eb20c0d06a6191066d70527b5af29bbb5107c850
RSW <rswpub@gmail.com>
2022-07-14 11:00:08 -0600

    finish constraint checking

--- LeetCode-11-ContainerWithMostWater/Class1.cs
+++ LeetCode-11-ContainerWithMostWater/Class1.cs
@@ -1,10 +1,36 @@
 ﻿namespace LeetCode_11_ContainerWithMostWater
 {
+    public class ConstraintViolationException : Exception
+    {
+    }
+
     public class Class1
     {
         public int MaxArea(int[] height)
         {
+            // Validate core constraints first
+            CheckConstraints(height);
 
+            throw new NotImplementedException();
+        }
+
+        private void CheckConstraints(int[] height)
+        {
+            // Constraints:
+            //
+            //    n == height.length
+            //    2 <= n <= 10**5
+            //    0 <= height[i] <= 10**4
+
+            if ((height.Length < 2) || (height.Length > 100000))
+            {
+                throw new ConstraintViolationException();
+            }
+
+            if ((height.Min() < 0) || (height.Max() > 10000))
+            {
+                throw new ConstraintViolationException();
+            }
         }
 
     }
--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,17 +1,54 @@
+using LeetCode_11_ContainerWithMostWater;
+
 namespace UnitTests
 {
     public class ProvideArrayOfIntegers
     {
         [Theory]
-        [InlineData(new int[] {1,8,6,2,5,4,8,3,7 }, 49)]
+        [InlineData(new int[] { 1, 1 }, 1)]
+        [InlineData(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
         public void ReturnsMaxArea(int[] heightArray, int expectedResult)
         {
 
             // Arrange
 
             // Act
+            int actualResult = new Class1().MaxArea(heightArray);
 
             // Assert
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1 })]
+        [InlineData(new int[] { -1, 1 })]
+        [InlineData(new int[] { 1, 10001 })]
+        public void ThrowsConstraintViolationExceptionWhenConstraintsViolated(int[] heightArray)
+        {
+
+            // Arrange
+
+            // Act
+            Action action = () => new Class1().MaxArea(heightArray);
+
+            // Assert
+            Assert.Throws<ConstraintViolationException>(action);
+
+        }
+
+        [Fact]
+        public void ThrowsConstraintViolationExceptionWhenArrayIsTooLarge()
+        {
+
+            // Arrange
+            int[] heightArray = new int[100001];
+
+            // Act
+            Action action = () => new Class1().MaxArea(heightArray);
+
+            // Assert
+            Assert.Throws<ConstraintViolationException>(action);
 
         }
     }
