4edc3f67fb3831e1f3f2b846ad14214dac9f954d
RSW <rswpub@gmail.com>
2022-07-14 11:36:42 -0600

    Add comments

--- LeetCode-11-ContainerWithMostWater/Class1.cs
+++ LeetCode-11-ContainerWithMostWater/Class1.cs
@@ -17,10 +17,15 @@
             // Choose the greatest from among them all.
             for (int i = 0; i < height.Length - 1; i++)
             {
+                // For the second pass through the array, start from the end of the array and work back.
+                // This gives us the opportunity to run the algorithm faster (performance improvement).
                 for (int j = height.Length - 1; j > i; j--)
                 {
                     if (height[j] >= height[i])
                     {
+                        // If the height of the 2nd line is higher than the 1st line, then we've found the max
+                        // area possible for any line paired with the first line, so no need to process this 
+                        // iteration of the loop any further.
                         int area = height[i] * (j - i);
 
                         if (area > maxArea)
@@ -32,6 +37,8 @@
                     }
                     else
                     {
+                        // If the height of the 2nd line is lower than the 1st line, we need to keep checking
+                        // the area of all other line pairs.
                         int area = height[j] * (j - i);
 
                         if (area > maxArea)
